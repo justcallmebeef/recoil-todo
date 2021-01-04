eaa20e1d81a6e4b0c99aeeacfa8fed1703eccb1e
justcallmebeef <bmitch@absencesoft.com>
2021-01-04 16:11:24 -0700

    added selectors and displayed stats

---
+++ src/Stats/TodoListStats.js
@@ -0,0 +1,23 @@
+import { useRecoilValue } from "recoil";
+
+import { todoListStatsState } from "./todoListStateState";
+
+export const TodoListStats = () => {
+  const {
+    totalNum,
+    totalCompletedNum,
+    totalUncompletedNum,
+    percentCompleted,
+  } = useRecoilValue(todoListStatsState);
+
+  const formattedPercentCompleted = Math.round(percentCompleted);
+
+  return (
+    <ul>
+      <li>Total items: {totalNum}</li>
+      <li>Items completed: {totalCompletedNum}</li>
+      <li>Items not completed: {totalUncompletedNum}</li>
+      <li>Percent completed: {formattedPercentCompleted}%</li>
+    </ul>
+  );
+};
--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,16 +1,18 @@
-import { todoListState } from "./todoListState";
 import { useRecoilValue } from "recoil";
 
 import { TodoItem } from "./TodoItem";
 import { TodoItemCreator } from "./TodoItemCreator";
+import { TodoListFilters } from "../Filter/TodoListFilters";
+import { filteredTodoListState } from "../Filter/filteredTodoListState";
+import { TodoListStats } from "../Stats/TodoListStats";
 
 export const TodoList = () => {
-  const todoList = useRecoilValue(todoListState);
+  const todoList = useRecoilValue(filteredTodoListState);
 
   return (
     <>
-      {/* <TodoListStats /> */}
-      {/* <TodoListFilters /> */}
+      <TodoListStats />
+      <TodoListFilters />
       <TodoItemCreator />
       {todoList.map((todoItem) => (
         <TodoItem key={todoItem.id} item={todoItem} />
